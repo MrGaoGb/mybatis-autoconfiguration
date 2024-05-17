0d98f47a68e283ce648fdeb337a7b97b24f6650f
Mr.Gao <862230476@qq.com>
2024-05-17 18:43:51 +0800

    自定义拦截器学习

---
+++ src/main/java/com/mrgao/xbqx/config/BeanConfig.java
@@ -0,0 +1,24 @@
+package com.mrgao.xbqx.config;
+
+import com.mrgao.xbqx.entity.Product;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Mr.Gao
+ * @date 2024/4/24 11:06
+ * @apiNote:
+ */
+//@Configuration
+//public class BeanConfig {
+//
+//    @Bean
+//    public Product twoProduct() {
+//        Product product = new Product();
+//        product.setId(1L);
+//        product.setPid("1111111");
+//        product.setPrdName("APPLE");
+//        product.setStorkCount(10000);
+//        return product;
+//    }
+//}
